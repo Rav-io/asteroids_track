9553e2b48d5e4db44ec6dc17ea16e4a8b44e9445
Rav-io <olejniczak.rafal99@gmail.com>
2024-01-19 15:21:46 +0100

    Implemented API fetch and calendar functionality

--- src/components/Asteroid.tsx
+++ src/components/Asteroid.tsx
@@ -1,64 +1,93 @@
 import React, { useState, useEffect } from "react";
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, SelectSingleEventHandler } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import './Asteroid.css';
+import _ from 'lodash';
 
 export const Asteroid = () => {
     const [asteroidsInfo, setAsteroidsInfo] = useState(null);
-    const [startDate, setStartDate] = useState<Date>();
-    let footer = <p>Pick a day.</p>;
-    let start_date = new Date().toJSON().slice(0, 10).replace(/-/g, "-");
+    const [startDate, setStartDate] = useState<Date>(new Date());
+    const [displayedStartDate, setDisplayedStartDate] = useState(new Date());
     const apiKey = "DEMO_KEY";
+
+
     useEffect(() => {
-        fetch(
-          `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&api_key=${apiKey}`
-        )
-          .then((res) => res.json())
-          .then((result) => {
-            setAsteroidsInfo(result.near_earth_objects[start_date]);
-          });
-      }, []);
-
-
+        const handleFetchData = async () => {
+            try {
+                let start_date = startDate.toJSON().slice(0, 10).replace(/-/g, "-");
+                const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&api_key=${apiKey}`);
+                const data = await response.json();
+        
+                if (data && data.near_earth_objects && data.near_earth_objects[start_date]) {
+                    setAsteroidsInfo(data.near_earth_objects[start_date]);
+                } else {
+                    console.error("Unexpected data structure in API response");
+                }
+            } catch (error) {
+                console.error("Error fetching or parsing data:", error);
+            }
+        }
+        const delay = 1000;
+        setTimeout(() => {
+            handleFetchData();
+        }, delay);
+      }, [startDate]);
 
     return (
         <>
+            <div id="calendar">
             <DayPicker
                 mode="single"
-                selected={startDate}
-                onSelect={setStartDate}
-                footer={footer}
+                selected={displayedStartDate}
+                onSelect={(day: Date | undefined) => {
+                    if (day) {
+                        const newStartDate = new Date(day.getTime() + 24 * 60 * 60 * 1000);
+                        setStartDate(newStartDate);
+                        setDisplayedStartDate(day);
+                    }
+                }}
             />
-            {asteroidsInfo !== null ? (
-                (asteroidsInfo as any[]).map((asteroid) => (
-                    <>
-                        <div className="name-div" key={asteroid.id}>
-                            <a href={asteroid?.nasa_jpl_url} className="name-link">
-                                Asteroid{" "}
-                                <div className="asteroid-name">{asteroid?.name}</div>
-                            </a>
-                        </div>
-                        <div className="diameter-div">
-                            <p>min diameter {asteroid.estimated_diameter.meters.estimated_diameter_min}</p>
-                        </div>
-                        <div className="diameter-div">
-                            <p>max diameter {asteroid.estimated_diameter.meters.estimated_diameter_max}</p>
-                        </div>
-                        <div className="hazardous-div">
-                            <p>hazardous? {asteroid.is_potentially_hazardous_asteroid ? "Yes" : "No"}</p>
-                        </div>
-                        <div className="approach-div">
-                            {asteroid.close_approach_data.map((approach:any) => (
-                            <div key={approach.epoch_date_close_approach}>
-                                <p>close_approach_date {approach.close_approach_date_full}</p>
-                                <p>relative_velocity {approach.relative_velocity.kilometers_per_hour} km/h</p>
-                                <p>miss_distance {approach.miss_distance.kilometers} km</p>
-                                <p>orbiting_body {approach.orbiting_body}</p>
+            </div>
+                {asteroidsInfo !== null ? (
+                    (asteroidsInfo as any[]).map((asteroid) => (
+                        <div id="info-box" key={asteroid.id}>
+                            <div className="name-div" key={asteroid.id}>
+                                <a href={asteroid?.nasa_jpl_url} className="name-link">
+                                    Asteroid{" "}
+                                    <div className="asteroid-name">{asteroid?.name}</div>
+                                </a>
                             </div>
-                            ))}
-                            <p>potential impact in the future? {asteroid.is_sentry_object ? "Yes" : "No"}</p>
+                            <p>
+                                <div className="info-text">
+                                    min diameter {asteroid.estimated_diameter.meters.estimated_diameter_min}
+                                </div>
+                            </p>
+                            <p>
+                                <div className="info-text">
+                                <p>max diameter {asteroid.estimated_diameter.meters.estimated_diameter_max}</p>
+                                </div>
+                            </p>
+                            <p>
+                                <div className="info-text">
+                                <p>hazardous? {asteroid.is_potentially_hazardous_asteroid ? "Yes" : "No"}</p>
+                                </div>
+                            </p>
+                            <div className="approach-div">
+                                {asteroid.close_approach_data.map((approach:any) => (
+                                <div key={approach.epoch_date_close_approach}>
+                                    <p>close_approach_date {approach.close_approach_date_full}</p>
+                                    <p>relative_velocity {approach.relative_velocity.kilometers_per_hour} km/h</p>
+                                    <p>miss_distance {approach.miss_distance.kilometers} km</p>
+                                    <p>orbiting_body {approach.orbiting_body}</p>
+                                </div>
+                                ))}
+                                <p>potential impact in the future? {asteroid.is_sentry_object ? "Yes" : "No"}</p>
+                            </div>
                         </div>
-                    </>
-                ))
-            ) : null}
+                    ))
+                ) : (
+                    <p>Loading...</p>
+                )}
         </>
     );
 }
